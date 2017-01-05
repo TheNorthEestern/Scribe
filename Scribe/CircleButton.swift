22a5f73fd43405b66ba409bf34fcf04af7b4a386
Kacy James <inquen@gmail.com>
2017-01-05 11:26:12 -0800

    Added font files, continued to tweak UI

---
+++ Scribe/CircleButton.swift
@@ -0,0 +1,31 @@
+//
+//  CircleButton.swift
+//  Scribe
+//
+//  Created by Kacy James on 1/5/17.
+//  Copyright © 2017 Student Driver. All rights reserved.
+//
+
+import UIKit
+
+
+
+@IBDesignable class CircleButton : UIButton {
+    @IBInspectable var cornerRadius: CGFloat = 20.0 {
+        didSet {
+            setupView()
+        }
+    }
+}
+
+extension CircleButton {
+    override func prepareForInterfaceBuilder() {
+        setupView()
+    }
+    
+    func setupView() {
+        layer.cornerRadius = cornerRadius
+        layer.borderWidth = 5.0
+        layer.borderColor = UIColor.red.cgColor
+    }
+}
